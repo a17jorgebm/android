f8f563d134c33fb0340b3f7e4280e4760da4cb43
jorge <a17jorgebm@iessanclemente.net>
2024-09-18 13:45:22 +0200

    botons...

--- tema1/app/src/main/java/com/example/ud01_02_lista_animales/MainActivity.kt
+++ tema1/app/src/main/java/com/example/ud01_02_lista_animales/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ud01_02_lista_animales
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val btnSend=findViewById<Button>(R.id.btnSend)
+        btnSend.setOnKeyListener();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
