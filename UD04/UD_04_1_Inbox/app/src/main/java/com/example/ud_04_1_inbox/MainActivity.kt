a57400d8f2efa5697c191a0041cf08616680100f
jorge <a17jorgebm@iessanclemente.net>
2024-10-28 12:59:10 +0100

    proyecto espacio

--- UD04/UD_04_1_Inbox/app/src/main/java/com/example/ud_04_1_inbox/MainActivity.kt
+++ UD04/UD_04_1_Inbox/app/src/main/java/com/example/ud_04_1_inbox/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -16,6 +17,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,14 +31,25 @@ class MainActivity : AppCompatActivity() {
         }
         val actionBar=findViewById<MaterialToolbar>(R.id.materialToolBar)
         setSupportActionBar(actionBar)
+
+        //pillamos drawer layout
+        val drawerLayout=findViewById<DrawerLayout>(R.id.drawerMain)
+
         val navegadorFragmentos= supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment
         val navController= navegadorFragmentos.navController
-        var appBarConf=  AppBarConfiguration.Builder(navController.graph).build()
-        actionBar.setupWithNavController(navController, appBarConf)
+        var appBarConf=  AppBarConfiguration.Builder(navController.graph)
+
+        appBarConf.setOpenableLayout(drawerLayout) //antes de facer o build da barra para añadir ese menu lateral
+
+        //navigationView do menu lateral
+        val menuLateral=drawerLayout.findViewById<NavigationView>(R.id.nav_side)
+        menuLateral.setupWithNavController(navController)
+
+        actionBar.setupWithNavController(navController, appBarConf.build())
 
         //de esta maneira, facemos esa barra a por defecto da aplicacion
         val actionBarBottom=findViewById<BottomNavigationView>(R.id.botton_navigation)
-        actionBarBottom.setupWithNavController(navController)
+        actionBarBottom.setupWithNavController(navController) //e pasamoslle o navigation pa que funcione
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
