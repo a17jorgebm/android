38541922b3a54d2d208b0530567e556ad2cb8ebd
jorge <a17jorgebm@iessanclemente.net>
2024-11-27 13:38:11 +0100

    AAAAAA

--- GestionResiduosProyecto/app/src/main/java/com/example/gestionresiduosproyecto/MainActivity.kt
+++ GestionResiduosProyecto/app/src/main/java/com/example/gestionresiduosproyecto/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.gestionresiduosproyecto
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +26,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //pillamos o navegador de fragmentos
+        val fragmentNavigator=supportFragmentManager.findFragmentById(R.id.fragment_container_view_main) as NavHostFragment
+        val fragmentNavigationController=fragmentNavigator.navController
+
+
+        val actionBottomBar=findViewById<BottomNavigationView>(R.id.menu_bottom_main)
+        actionBottomBar.setupWithNavController(fragmentNavigationController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragment_container_view_main)
+        NavigationUI.onNavDestinationSelected(item, navController)
+        return super.onOptionsItemSelected(item)
     }
 }
